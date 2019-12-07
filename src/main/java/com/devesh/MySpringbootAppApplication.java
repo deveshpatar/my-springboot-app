c639dce37d7d928ca2ded012f1add612dc774a90
Devesh Kumar Patar <deveshpatar@gmail.com>
2019-12-08 00:56:12 +0530

    my-springboot-app

--- src/main/java/com/devesh/MySpringbootAppApplication.java
+++ src/main/java/com/devesh/MySpringbootAppApplication.java
@@ -1,5 +1,8 @@
 package com.devesh;
 
+/**
+ * @author devesh
+ */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/devesh/service/IArticleService.java
@@ -0,0 +1,13 @@
+package com.devesh.service;
+
+import java.util.List;
+
+import com.devesh.entity.Article;
+
+public interface IArticleService {
+     List<Article> getAllArticles();
+     Article getArticleById(int articleId);
+     boolean addArticle(Article article);
+     void updateArticle(Article article);
+     void deleteArticle(int articleId);
+}
