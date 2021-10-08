abc89831205ea46ce9397f1913873de59ad62088
Devesh Kumar Patar <deveshpatar@gmail.com>
2021-10-09 01:01:38 +0530

    Update MySpringBootServiceImpl.java

--- src/main/java/com/devesh/service/MySpringBootServiceImpl.java
+++ src/main/java/com/devesh/service/MySpringBootServiceImpl.java
@@ -1,14 +1,28 @@
 package com.devesh.service;
 
+import java.io.IOException;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devesh.repository.CustomerRepository;
+import com.devesh.utils.Constants;
+import com.devesh.utils.LanguageUtil;
+import com.devesh.utils.WindowsTimeZoneAPI;
 
 @Service
 public class MySpringBootServiceImpl {
 	
 	@Autowired
 	CustomerRepository customerRepository;
+	
+	public Map<String, String> getSystemTimezoneList() throws IOException, InterruptedException{
+		return WindowsTimeZoneAPI.getTimeZoneMap(Constants.TIMEZONE_LIST_ALL_CMD);
+	}
+	
+	public Map<String, String> getSystemInstalledLanguageList() throws IOException, InterruptedException {
+		return LanguageUtil.getInstalledLanguageList((Constants.INSTALLED_LANGUAGE_LIST_PS));
+	}
 
 }
